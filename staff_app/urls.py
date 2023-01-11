1e9612cba92c27fc16e3f42c9fe3ce5e4ac5d424
Linyx67 <linyx67@hotmail.com>
2023-01-11 04:29:26 -0400

    staff profile

--- staff_app/urls.py
+++ staff_app/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('profile_update', views.profile_update, name="profile-update"),
     path('apply_leave', views.apply_leave, name="apply-leave"),
     path('apply_leave_save', views.apply_leave_save, name="apply-leave-save"),
+    path('Awards', views.apply_leave_save, name="apply-leave-save"),
+    path('Publications', views.apply_leave_save, name="apply-leave-save"),
 
 ]
