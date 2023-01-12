b97f457a2376323069c5a2d67a0a88413361eaad
Linyx67 <linyx67@hotmail.com>
2023-01-12 04:37:18 -0400

    added bootstrap to forms

--- staff_app/forms.py
+++ staff_app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import UserChangeForm
 import datetime
 from hris_app.models import CustomUser
 from .models import (
@@ -14,14 +15,27 @@ from .models import (
 
 
 class EmployeeCreateForm(forms.ModelForm):
-    image = forms.ImageField(required=False, widget=forms.FileInput(
-        attrs={'onchange': 'previewImage(this);'}))
+    image = forms.ImageField(required=False)
 
     class Meta:
         model = Employee
         exclude = ['is_blocked', 'is_deleted', 'created', 'updated', 'user']
         widgets = {
-            'bio': forms.Textarea(attrs={'cols': 100, 'rows': 5})
+            'bio': forms.Textarea(attrs={'cols': 100, 'rows': 5, 'class': 'form-control'}),
+            'firstname': forms.TextInput(attrs={'class': 'form-control'}),
+            'lastname': forms.TextInput(attrs={'class': 'form-control'}),
+            'othername': forms.TextInput(attrs={'class': 'form-control'}),
+            'tel': forms.TextInput(attrs={'class': 'form-control'}),
+            'ssnitnumber': forms.TextInput(attrs={'class': 'form-control'}),
+            'tinnumber': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'birthday': forms.DateInput(attrs={'placeholder': 'YYYY-MM-DD', 'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'onchange': 'previewImage(this);',
+                                            'class': 'form-control-file'}),
+            'title': forms.Select(attrs={'class': 'form-control'}),
+            'sex': forms.Select(attrs={'class': 'form-control'}),
+            'employeetype': forms.Select(attrs={'class': 'form-control'}),
+            'tel': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'eg. +18682224444'})
         }
 
     def clean_email(self, *args, **kwargs):
