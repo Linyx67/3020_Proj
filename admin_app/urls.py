f146139198a5848a0dbde3826f1416cba8b578bd
Linyx67 <linyx67@hotmail.com>
2023-01-17 01:54:07 -0400

    started admin

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
+from . import views
 
+app_name = 'admin_app'
 urlpatterns = [
-
+    path('', views.home, name="admin-home"),
+    path('employees/', views.employees, name="employees"),
+    path('leaves/', views.leaves_list, name="leaves"),
 ]
