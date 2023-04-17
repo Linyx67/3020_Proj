c5922f709608f6d29a8893c0f637465e87daabbc
Linyx67 <94486863+Linyx67@users.noreply.github.com>
2023-04-16 23:09:31 -0400

    empty commit

--- jobs/updater.py
+++ jobs/updater.py
@@ -5,5 +5,5 @@ from .jobs import rfi_api
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(rfi_api, 'interval', days=7)
+    scheduler.add_job(rfi_api, 'interval', days=6)
     scheduler.start()
